46e59e93413cb0333df1772670d18a4098042665
sarzhh <146794146+sarzhh@users.noreply.github.com>
2024-08-24 14:19:33 +0000

    calculate selling price

--- stock-piper.rb
+++ stock-piper.rb
@@ -9,23 +9,45 @@ end
 
 def stock_picker(stock)
   sellingprice_and_buyingprice = []
+  sellingpriceindex_and_buyingpriceindex = []
   min_index = 0
+  buying_price_index = 0
   new_price = 0
+  new_price_index = 0
   buying_price = 0
-
+  selling_price = 0
+  selling_price_index = 0
+  
   stock.each_with_index do |price, index|
     if price < stock[min_index]
       new_price = price
-      
+      new_price_index = index
+
       if new_price < stock[index + 1]
         buying_price = new_price
+        buying_price_index = new_price_index
         break
       end 
     end 
-   
   end
-  sellingprice_and_buyingprice.push(buying_price)
-  sellingprice_and_buyingprice
+
+  stock[buying_price_index..-1].each_with_index do |price, index|
+    if price > stock[buying_price_index]
+      new_price = price
+      new_price_index = index + buying_price_index
+      
+      if new_price > stock[index + buying_price_index + 1]
+        selling_price = new_price
+        selling_price_index = new_price_index
+        break
+      end
+    end
+
+  end
+
+  sellingprice_and_buyingprice.push(buying_price, selling_price)
+  sellingpriceindex_and_buyingpriceindex.push(buying_price_index, selling_price_index)
+  [sellingprice_and_buyingprice, sellingpriceindex_and_buyingpriceindex]
   
 end
 puts stock_picker(stock).inspect
